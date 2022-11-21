612b0371e3001b9c485e0798e0dba15a378fcf47
Arjun2002tiwari <Arjarjun200@gmail.com>
2022-11-21 22:58:32 +0530

    changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,7 +18,7 @@ export default function Form(props) {
     const [isNews ,setNew]=useState(true);
     const [fstatus,setFstatus]=useState(false);
     const [img,SetImage]=useState(null);
-    const [id,setId]=useState(0);
+    //const [id,setId]=useState(0);
     //const [isKey,setKey]=useState(true);
     let name,value;
     const handleInputs = (e)=>{
@@ -67,7 +67,7 @@ export default function Form(props) {
                 .then((result) => {
                     console.log(result.id);
                     //id=result.id;
-                    setId(result.id);
+                    console.log(result.id);
                     setNew(false);
                 })
                 .catch((error) => {
@@ -79,89 +79,71 @@ export default function Form(props) {
             props.showAlert("Button is disabled!","primary");
         }
     }
-    const keyCall=async (e)=>{
-        //console.log(data);
-            const url="https://enews-api.herokuapp.com/api/keywords"
-            await fetch(url, {
-                method: 'POST',
-                body: JSON.stringify({'id':id,'k1':news.k1,'k2':news.k2,'k3':news.k3,'k4':news.k4,'k5':news.k5}),
-                })
-                .then((response) => response.json())
-                .then((result) => {
-                    //setKey(false);
-                    console.log(result);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
-    }
+    // const keyCall=async (e)=>{
+    //     //console.log(data);
+    //         const url="https://enews-api.herokuapp.com/api/keywords"
+    //         await fetch(url, {
+    //             method: 'POST',
+    //             body: JSON.stringify({'id':id,'k1':news.k1,'k2':news.k2,'k3':news.k3,'k4':news.k4,'k5':news.k5}),
+    //             })
+    //             .then((response) => response.json())
+    //             .then((result) => {
+    //                 //setKey(false);
+    //                 console.log(result);
+    //             })
+    //             .catch((error) => {
+    //                 console.error('Error:', error);
+    //             });
+    // }
     
   return(
     isNews?
     <>
     <form>
-    <div className="form-group">
+    <div classNameName="form-group">
         <label htmlFor="title">Title</label>
-        <input type="text" className="form-control" id="title" name="title" value={news.title} onChange={handleInputs}/>
+        <input type="text" classNameName="form-control" id="title" name="title" value={news.title} onChange={handleInputs}/>
     </div>
     
-    <div className="form-group">
+    <div classNameName="form-group">
         <label htmlFor="article">Article</label>
-        <textarea className="form-control" id="article" rows="3" name="article" value={news.article} onChange={handleInputs}></textarea>
+        <textarea classNameName="form-control" id="article" rows="3" name="article" value={news.article} onChange={handleInputs}></textarea>
     </div>
 
-    <div className="form-group">
+    <div classNameName="form-group">
         <label htmlFor="category">Category</label>
-        <input type="text" className="form-control" id="category" name="category" value={news.category} onChange={handleInputs}/>
+        <input type="text" classNameName="form-control" id="category" name="category" value={news.category} onChange={handleInputs}/>
     </div>
 
-    <div className="form-group">
+    <div classNameName="form-group">
         <label htmlFor="keyword">Source</label>
-        <input type="text" className="form-control" id="source" name="source" value={news.source} onChange={handleInputs}/>
+        <input type="text" classNameName="form-control" id="source" name="source" value={news.source} onChange={handleInputs}/>
     </div>
 
-    <div className="card" style={{"marginTop":"20px"}}>
-        <div className="card-body">
-            <div className="form-group">
+    <div classNameName="card" style={{"marginTop":"20px"}}>
+        <div classNameName="card-body">
+            <div classNameName="form-group">
                 <label htmlFor="exampleFormControlFile1">Image</label>
-                <input type="file" className="form-control-file" id="image" onChange={handleImage}/>
+                <input type="file" classNameName="form-control-file" id="image" onChange={handleImage}/>
             </div>     
         </div>
     </div>
 </form>
-<div className="text-center">
-    <button type="button" className="btn btn-primary btn-lg btn-block" onClick={finalCall} style={{'marginTop':"10px"}}>Next!</button>
+<div classNameName="text-center">
+    <button type="button" classNameName="btn btn-primary btn-lg btn-block" onClick={finalCall} style={{'marginTop':"10px"}}>Next!</button>
 </div>
 </>:<>
-<form>
-    <div className="form-group">
-        <label htmlFor="k1">Keyword-1</label>
-        <input type="text" className="form-control" id="k1" name="k1" value={news.k1} onChange={handleInputs}/>
-    </div>
-    
-    <div className="form-group">
-        <label htmlFor="k2">Keyword-2</label>
-        <input type="text" className="form-control" id="k2" name="k2" value={news.k2} onChange={handleInputs}/>
-    </div>
-
-    <div className="form-group">
-        <label htmlFor="k3">Keyword-3</label>
-        <input type="text" className="form-control" id="k3" name="k3" value={news.k3} onChange={handleInputs}/>
-    </div>
-
-    <div className="form-group">
-        <label htmlFor="k4">Keyword-4</label>
-        <input type="text" className="form-control" id="k4" name="k4" value={news.k4} onChange={handleInputs}/>
-    </div>
-
-    <div className="form-group">
-        <label htmlFor="k5">Keyword-5</label>
-        <input type="text" className="form-control" id="k5" name="k5" value={news.k5} onChange={handleInputs}/>
-    </div>
-</form>
-<div className="text-center">
-    <button type="button" className="btn btn-primary btn-lg btn-block" onClick={keyCall} style={{'marginTop':"10px"}}>Submit!</button>
-</div>
+    <div className="jumbotron text-center">
+    <h1 className="display-3">Thank You!</h1>
+    <p className="lead"><strong>Please check your Database!</strong></p>
+    <hr/>
+    <p>
+      <br/>
+    </p>
+    <p className="lead">
+      <a className="btn btn-primary btn-sm" href="/" role="button">Continue to homepage</a>
+    </p>
+  </div>
 </>
 )
 }
